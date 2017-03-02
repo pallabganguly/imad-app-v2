85972d8b61aff6ecfe723a1a3aa4ab691c19fe19
Pallab Ganguly <pallabkumarganguly@gmail.com>
2017-03-02 20:42:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/question.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'question.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
